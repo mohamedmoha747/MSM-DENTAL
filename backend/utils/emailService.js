44e028b3daae70a0e382e131ff656c36a51ca20b
mohamedmoha747 <mohamedharun76623@gmail.com>
2026-05-03 15:21:55 +0530

    dd

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,13 +1,14 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp-relay.brevo.com',
+  host: "smtp-relay.brevo.com", // or smtp.gmail.com
   port: 587,
   secure: false,
   auth: {
     user: process.env.BREVO_USER,
     pass: process.env.BREVO_PASS
-  }
+  },
+  family: 4 // 🔥 FORCE IPv4 (THIS FIXES ERROR)
 });
 
 
